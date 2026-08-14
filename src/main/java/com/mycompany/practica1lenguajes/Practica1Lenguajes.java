2f35ccf2e7fcc8040ba1e3538692ac8d77e7c3e1
FernanGonzalez202331283 <fernangonzalez202331283@cunoc.edu.gt>
2026-08-14 13:00:57 -0600

    codigo inicial

---
+++ src/main/java/com/mycompany/practica1lenguajes/Practica1Lenguajes.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practica1lenguajes;
+
+/**
+ *
+ * @author fernan
+ */
+public class Practica1Lenguajes {
+
+    public static void main(String[] args) {
+        System.out.println("=========================");
+        System.out.println("   ANALIZADOR PROMPTZAL  ");
+        System.out.println("=========================");
+    }
+}
